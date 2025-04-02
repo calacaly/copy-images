31be2eaa2101f9909f8bc9e769219581746dc0ff
calacaly <calacaly@foxmail.com>
2025-04-02 17:22:24 +0800

    update

--- sync.sh
+++ sync.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e  # 确保脚本遇到错误时立即退出
+set -x
 
 # 解析 auths.yaml 并登录到每个 registry
 for auth_entry in $(yq e '.auths[]' auths.yaml); do
     domain=$(echo "$auth_entry" | jq '.domain')
-    username_secret=$(echo "$auth_entry" | jq '.username')
-    password_secret=$(echo "$auth_entry" | jq '.password')
-
-    username=$(echo "$username_secret" | base64 -d)
-    password=$(echo "$password_secret" | base64 -d)
+    username=$(echo "$auth_entry" | jq '.username' | base64 -d)
+    password=$(echo "$auth_entry" | jq '.password' | base64 -d)
 
     if [[ -z "$username" || -z "$password" ]]; then
         echo "Error: Could not find credentials for domain $domain"
@@ -20,7 +18,7 @@ for auth_entry in $(yq e '.auths[]' auths.yaml); do
 done
 
 
-# 解析 images.yaml 并同步镜像
+# 解析 images.yaml 并同步镜像`
 for image_entry in $(yq e '.images[]' images.yaml); do
     source=$(echo "$image_entry" | jq '.source')
     target=$(echo "$image_entry" | jq '.target')
