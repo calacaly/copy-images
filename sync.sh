1c1b4d4291fd7e21756aa2c969011c57ad6ef6f1
calacaly <calacaly@foxmail.com>
2025-04-02 17:00:18 +0800

    update

--- sync.sh
+++ sync.sh
@@ -1,14 +1,21 @@
-# /bin/bash
+#!/bin/bash
 
-for auths in $(yq '.auths[]' auths.yaml); do
-    username=$(echo $auths | yq '.username')
-    password=$(echo $auths | yq '.password')
-    domain=$(echo $auths | yq '.domain')
-    skopeo login --username $username --password $password $domain
+# 解析 auths.yaml 并登录到每个 registry
+for auth in $(yq e '.auths | to_entries[] | [.key, .value.username, .value.password]' auths.yaml -o=json | jq -c '.'); do
+    domain=$(echo $auth | jq -r '.[0]')
+    username=$(echo $auth | jq -r '.[1]')
+    password=$(echo $auth | jq -r '.[2]')
+    
+    echo "Logging into $domain as $username"
+    echo "$password" | skopeo login --username "$username" --password-stdin "$domain"
 done
 
-for images in $(yq '.images[]' images.yaml); do
-    source=$(echo $images | yq '.source')
-    target=$(echo $images | yq '.target')
-    skopeo sync --src docker --dest docker $source $target
+# 解析 images.yaml 并同步镜像
+for image in $(yq e '.images | to_entries[] | [.key, .value.source, .value.target]' images.yaml -o=json | jq -c '.'); do
+    name=$(echo $image | jq -r '.[0]')
+    source=$(echo $image | jq -r '.[1]')
+    target=$(echo $image | jq -r '.[2]')
+    
+    echo "Syncing image $name from $source to $target"
+    skopeo sync --src docker --dest docker "$source" "$target"
 done
