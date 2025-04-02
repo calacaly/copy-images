ec38318a93d9f6b46acfe55b5a24ccf25557d512
calacaly <calacaly@foxmail.com>
2025-04-02 17:46:56 +0800

    update

--- sync.sh
+++ sync.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e  # 确保脚本遇到错误时立即退出
-set -x
 
 # 解析 auths.yaml 并登录到每个 registry
 length=$(yq '.auths | length' auths.yaml)
 for ((i = 0; i < length; i++)); do
     username_key=$(yq ".auths[$i].username" auths.yaml)
     password_key=$(yq ".auths[$i].password" auths.yaml)
-    username=$(echo "${!username_key}" | base64 -d)
-    password=$(echo "${!password_key}" | base64 -d)
+    username_secret=${!username_key}
+    password_secret=${!password_key}
+    username=$(echo "$username_secret" | base64 -d)
+    password=$(echo "$password_secret" | base64 -d)
     domain=$(yq ".auths[$i].domain" auths.yaml)
     if [[ -z "$username" || -z "$password" ]]; then
         echo "Error: Could not find credentials for domain $domain"
@@ -16,7 +17,7 @@ for ((i = 0; i < length; i++)); do
     fi
 
     echo "Logging into $domain as $username"
-    #echo "$password" | skopeo login --username "$username" --password-stdin "$domain"
+    echo "$password" | skopeo login --username "$username" --password-stdin "$domain"
 done
 
 
