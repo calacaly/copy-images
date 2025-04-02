859d58125c50f10049818caab1075341df1294e0
calacaly <calacaly@foxmail.com>
2025-04-02 17:44:43 +0800

    update

--- sync.sh
+++ sync.sh
@@ -3,26 +3,28 @@ set -e  # 确保脚本遇到错误时立即退出
 set -x
 
 # 解析 auths.yaml 并登录到每个 registry
-for auth_entry in $(yq e '.auths[]' auths.yaml); do
-    domain=$(echo "$auth_entry" | jq '.domain')
-    username=$(echo "$auth_entry" | jq '.username' | base64 -d)
-    password=$(echo "$auth_entry" | jq '.password' | base64 -d)
-
+length=$(yq '.auths | length' auths.yaml)
+for ((i = 0; i < length; i++)); do
+    username_key=$(yq ".auths[$i].username" auths.yaml)
+    password_key=$(yq ".auths[$i].password" auths.yaml)
+    username=$(echo "${!username_key}" | base64 -d)
+    password=$(echo "${!password_key}" | base64 -d)
+    domain=$(yq ".auths[$i].domain" auths.yaml)
     if [[ -z "$username" || -z "$password" ]]; then
         echo "Error: Could not find credentials for domain $domain"
         exit 1
     fi
 
     echo "Logging into $domain as $username"
-    echo "$password" | skopeo login --username "$username" --password-stdin "$domain"
+    #echo "$password" | skopeo login --username "$username" --password-stdin "$domain"
 done
 
 
 # 解析 images.yaml 并同步镜像`
-for image_entry in $(yq e '.images[]' images.yaml); do
-    source=$(echo "$image_entry" | jq '.source')
-    target=$(echo "$image_entry" | jq '.target')
-
+length=$(yq '.auths | length' auths.yaml)
+for ((i = 0; i < length; i++)); do
+    source=$(yq ".images[$i].source" images.yaml)
+    target=$(yq ".images[$i].target" images.yaml)
     echo "Syncing image from $source to $target"
     skopeo sync --src docker --dest docker "$source" "$target"
 done
